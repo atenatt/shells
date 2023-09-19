08f301eaeec0b793c35ae247e7f781832ab86b05
Victor Pedro <56831201+atenatt@users.noreply.github.com>
2023-09-19 03:46:12 -0300

    Update tooner.sh
    Alteração no código com melhorias

--- tooner.sh
+++ tooner.sh
@@ -1,49 +1,58 @@
 #!/usr/bin/env bash
 ########################################################################################
 #
-# tooner.sh - Script feito para verificar o status do tooner das impressoras LEXMARK 
-#
+# tooner.sh - Script feito para verificar o status do tooner das impressoras LEXMARK
 #
 # Desenvolvido por: Victor Pedro (victor.boas@fatec.sp.gov.br)
 #
 # Shell desenvolvido com o intuito de agilizar a verificação do status de tooner de impressoras lexmark.
-# Data da ultima atualização: 19/09/23
 #
-# Utilização: sh tooner.sh
+# Data da ultima atualização: 19/09/23
 #
 #########################################################################################
 
 
-movecursor() {                   # Função para mover cursor
-    tput cup $1 $2 ;
+#!/bin/bash
+
+# Nome do arquivo de log
+LOG_FILE="status_impressoras.log"
+
+# Constantes para IPs das Impressoras
+declare -a IP_IMPRESSORAS=("192.168.0.10" "192.168.0.11" "192.168.0.12" "192.168.0.13")
+
+# Construir URLs das Impressoras
+URLS=()
+for ip in "${IP_IMPRESSORAS[@]}"; do
+    URLS+=("http://$ip/cgi-bin/dynamic/printer/PrinterStatus.html")
+done
+
+# Função para mover cursor
+movecursor() {
+    tput cup "$1" "$2"
 }
 
-IP_IMP_001="192.168.0.10"        # Variaveis que definem o IP das Impressoras
-IP_IMP_002="192.168.0.11"
-IP_IMP_003="192.168.0.12"
-IP_IMP_004="192.168.0.13"
+# Função para escrever no arquivo de log
+log() {
+    local message="$1"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $message" >> "$LOG_FILE"
+}
 
-IMP_001="http://$IP_IMP_001/cgi-bin/dynamic/printer/PrinterStatus.html"
-IMP_002="http://$IP_IMP_002/cgi-bin/dynamic/printer/PrinterStatus.html"
-IMP_003="http://$IP_IMP_003/cgi-bin/dynamic/printer/PrinterStatus.html"
-IMP_004="http://$IP_IMP_004/cgi-bin/dynamic/printer/PrinterStatus.html"
+# Verificar status dos toners
+statuses=()
+for url in "${URLS[@]}"; do
+    status=$(curl -s "$url" | grep "Cartucho Preto" | cut -c 38-42 | tr -s ' ')
+    statuses+=("$status")
+done
 
-var1=$(curl -s $IMP_001 | grep "Cartucho Preto" | cut -c 38-42 | tr -s ' ')
-var2=$(curl -s $IMP_002 | grep "Cartucho Preto" | cut -c 38-42 | tr -s ' ')
-var3=$(curl -s $IMP_003 | grep "Cartucho Preto" | cut -c 38-42 | tr -s ' ')
-var4=$(curl -s $IMP_004 | grep "Cartucho Preto" | cut -c 38-42 | tr -s ' ')
-
-# Imprimindo resultado #
-
+# Imprimir resultados e registrar no arquivo de log
 clear
+movecursor 2 30; echo -e "\033[37;01mSTATUS DOS TONERS\033m"
 
-movecursor 2 30; echo -e "\033[37;01mSTATUS DOS TOONERS\033m"
+for i in "${!statuses[@]}"; do
+    echo "IMPRESSORA $((i+1)): ${statuses[i]}"
+    log "Status da Impressora $((i+1)): ${statuses[i]}"
+done
 
-echo IMPRESSORA 001: $var1
-echo IMPRESSORA 002: $var2
-echo IMPRESSORA 003: $var3
-echo IMPRESSORA 004: $var4
-
-    movecursor 22 3; read -n1 -p "Aperte qualquer tecla para finalizar: "
+# Aguardar por entrada do usuário
+movecursor 22 3; read -n1 -p "Pressione qualquer tecla para finalizar: "
 clear
-
